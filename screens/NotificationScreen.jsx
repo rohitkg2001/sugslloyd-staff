775e23045eb3d7f7c75557a4d915deb53604c124
Mihir Kumar Mishra <mkm20170@gmail.com>
2024-11-16 18:13:00 +0530

    Changes

--- screens/NotificationScreen.jsx
+++ screens/NotificationScreen.jsx
@@ -1,4 +1,4 @@
-import { View, FlatList } from "react-native";
+import { View } from "react-native";
 import { Avatar, List, Divider } from "react-native-paper";
 import ContainerComponent from "../components/ContainerComponent";
 import MyHeader from "../components/header/MyHeader";
@@ -7,8 +7,8 @@ import { TouchableOpacity } from "react-native";
 import { styles } from "../styles/components.styles";
 import { H2, H5, P } from "../components/text";
 import { useFonts } from "expo-font";
-//import MyFlatList from "../components/utility/MyFlatList";
-
+import MyFlatList from "../components/utility/MyFlatList";
+import { PRIMARY_COLOR } from "../styles/constant";
 export default function NotificationScreen() {
   const renderItem = ({ item }) => (
     <TouchableOpacity>
@@ -19,7 +19,7 @@ export default function NotificationScreen() {
           <Avatar.Icon
             size={40}
             icon={item.icon}
-            style={{ backgroundColor: "#2b87b0" }}
+            backgroundColor={PRIMARY_COLOR}
           />
         )}
         right={() => <H2 style={styles.time}>{item.time}</H2>}
@@ -37,7 +37,7 @@ export default function NotificationScreen() {
         icon={"search-outline"}
       />
       <View>
-        <FlatList
+        <MyFlatList
           data={notifications}
           renderItem={renderItem}
           keyExtractor={(item) => item.id}
