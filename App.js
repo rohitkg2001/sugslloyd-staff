186fa382f1811403c0c75632b0c08692f9224ebb
Mihir Kumar Mishra <mkm20170@gmail.com>
2024-11-20 01:44:15 +0530

    Changes in code

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import "react-native-gesture-handler"; //Don't ever remove this line
 import { useEffect } from "react";
 import { PaperProvider } from "react-native-paper";
 import MyNavigationContainer from "./navigation/MyNavigationContainer";
-import { checkPermissions } from "./utils/checkPermissions";
+
 
 export default function App() {
   return (
