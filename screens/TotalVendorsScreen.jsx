9a06d15c33c24acd546329f65fd0d52f76751b6f
Mihir Kumar Mishra <mkm20170@gmail.com>
2024-11-21 01:33:13 +0530

    Changes

--- screens/TotalVendorsScreen.jsx
+++ screens/TotalVendorsScreen.jsx
@@ -1,145 +1,66 @@
-import { useState } from "react";
-import { View, Alert } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import { fakeDelete, totalVendorsData } from "../utils/faker";
 import ContainerComponent from "../components/ContainerComponent";
 import MyHeader from "../components/header/MyHeader";
+import { spacing, styles } from "../styles";
 import SearchBar from "../components/input/SearchBar";
-import Filter from "../components/filters";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import MyFlatList from "../components/utility/MyFlatList";
+import NoRecord from "./NoRecord";
 import Button from "../components/buttons/Button";
 import ClickableCard from "../components/card/ClickableCard";
-import { SCREEN_WIDTH, spacing, styles } from "../styles";
-import { totalVendorsData } from "../utils/faker";
 
-const TotalVendorsScreen = ({navigation}) => {
-  const [searchText, setSearchText] = useState("");
-  const [isMenuVisible, setIsMenuVisible] = useState(false);
-  const [filteredVendors, setFilteredVendors] = useState(totalVendorsData);
+export default function TotalVendorsScreen({ navigation , route }) {
+  const { pageTitle = "Vendor Management", data = totalVendorsData } =
+    route.params || {};
 
-
-  const toggleMenu = () => {
-    setIsMenuVisible(!isMenuVisible);
-  };
-
-  const handleEdit = (item) => {
-    navigation.navigate("EditDetailsScreen", {
-      vendor: item,
-      formType: "vendor",
-    });
+  const handleViewDetails = (vendor) => {
+    navigation.navigate("ViewDetailScreen", { vendor });
   };
 
   const handleDelete = (vendorToDelete) => {
-    Alert.alert(
-      "Confirm Delete",
-      `Are you sure you want to delete ${vendorToDelete.name}?`,
-      [
-        { text: "Cancel", style: "cancel" },
-        {
-          text: "Delete",
-          style: "destructive",
-          onPress: () => {
-            const updatedVendors = filteredVendors.filter(
-              (item) => item.id !== vendorToDelete.id
-            );
-            setFilteredVendors(updatedVendors);
-          },
-        },
-      ]
-    );
+    fakeDelete({
+      title: "Error!!!",
+      message: `You cannot delete ${vendorToDelete.name}. Please contact Admin!`,
+    });
   };
 
-  const handleViewDetails = (vendor) => {
-    navigation.navigate("ViewDetailScreen", {
-      site: vendor,
+  const handleEdit = (vendor) => {
+    navigation.navigate("EditDetailsScreen", {
+      vendor,
       formType: "vendor",
     });
   };
 
-  const filterVendors = (text) => {
-    setSearchText(text);
-    const filtered = totalVendorsData.filter((item) =>
-      item.name.toLowerCase().includes(text.toLowerCase())
-    );
-    setFilteredVendors(filtered);
-  };
-
-  const sortVendors = (sortOrder) => {
-    let sortedVendors = [...filteredVendors];
-
-    if (sortOrder === "alphabetical") {
-      sortedVendors.sort((a, b) => a.name.localeCompare(b.name));
-    } else if (sortOrder === "locationAsc") {
-      sortedVendors.sort((a, b) => a.location.localeCompare(b.location));
-    }
-
-    setFilteredVendors(sortedVendors);
-  };
-
-  const handleSortAlphabetically = () => {
-    sortVendors("alphabetical");
-    toggleMenu();
-  };
-
-  const handleSortByLocation = () => {
-    sortVendors("locationAsc");
-    toggleMenu();
-  };
-
-  const menuOptions = [
-    { label: "Search", onPress: () => console.log("Search clicked") },
-    { label: "Sort Alphabetically", onPress: handleSortAlphabetically },
-    { label: "Sort by Location", onPress: handleSortByLocation },
-  ];
-
   return (
     <ContainerComponent>
-      <View style={[spacing.mh1, { width: SCREEN_WIDTH - 16 }]}>
-        <MyHeader
-          title="Total Vendors"
-          isBack={true}
-          hasIcon={true}
-          icon={"ellipsis-vertical"}
-          onIconPress={toggleMenu}
-        />
-        <MyFlatList
-          data={filteredVendors}
-          loading={false}
-          renderItem={({ item }) => (
-            <ClickableCard
-              item={item}
-              handleViewDetails={handleViewDetails}
-              handleDelete={handleDelete}
-              handleEdit={handleEdit}
-              isVendor={true}
-            />
-          )}
-          keyExtractor={(item) => item.id.toString()}
-          contentContainerStyle={styles.list}
-          ListEmptyComponent={() => (
-            <NoRecord msg="Oops! No Vendors available. Create the new one." />
-          )}
-          ListHeaderComponent={() => (
-            <SearchBar
-              placeholder="Search by name, state or project code"
-              value={searchText}
-              onChangeText={filterVendors}
-            />
-          )}
-        />
-        <Button
-          style={styles.addButton}
-          onPress={() => navigation.navigate("VendorFormScreen")}
-        >
-          <Ionicons name="add" size={32} color="white" />
-        </Button>
-        <Filter
-          visible={isMenuVisible}
-          onClose={toggleMenu}
-          options={menuOptions}
-        />
-      </View>
+      <MyHeader title={pageTitle} isBack={true} hasIcon={true} />
+      <MyFlatList
+        data={data}
+        loading={false}
+        renderItem={({ item }) => (
+          <ClickableCard
+            item={item}
+            handleViewDetails={() => handleViewDetails(item)}
+            handleDelete={() => handleDelete(item)}
+            handleEdit={() => handleEdit(item)}
+            isVendor={true}
+          />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={[spacing.mh2, spacing.mt1]}
+        ListEmptyComponent={() => (
+          <NoRecord msg="Oops! No Vendors available. Create the new one." />
+        )}
+        ListHeaderComponent={() => (
+          <SearchBar placeholder="Search by name, state or project code" />
+        )}
+      />
+      <Button
+        style={styles.addButton}
+        onPress={() => navigation.navigate("VendorFormScreen")}
+      >
+        <Ionicons name="add" size={32} color="white" />
+      </Button>
     </ContainerComponent>
   );
-};
-
-export default TotalVendorsScreen;
+}
