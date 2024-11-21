1d0d215882b115835a7e7a884debbda0916c59b4
Rohit <rgupta6205@gmail.com>
2024-11-21 11:23:53 +0530

    setup reducer

--- screens/TotalEarningScreen.jsx
+++ screens/TotalEarningScreen.jsx
@@ -2,50 +2,20 @@ import { useState } from "react";
 import { View, TouchableOpacity } from "react-native";
 import { earnings } from "../utils/faker";
 import ContainerComponent from "../components/ContainerComponent";
-import { SCREEN_WIDTH, spacing } from "../styles";
+import { spacing } from "../styles";
 import { styles } from "../styles/components.styles";
 import MyHeader from "../components/header/MyHeader";
 import { H5, P } from "../components/text";
 import SearchBar from "../components/input/SearchBar";
 import MyFlatList from "../components/utility/MyFlatList";
 
-const TotalEarningScreen = () => {
-  const [searchText, setSearchText] = useState("");
+export default function TotalEarningScreen() {
   const [filteredEarnings, setFilteredEarnings] = useState(earnings);
-
- 
-  const filterEarnings = (text) => {
-    setSearchText(text);
-    const filtered = earnings.filter((earning) =>
-      earning.projectName.toLowerCase().includes(text.toLowerCase())
-    );
-    setFilteredEarnings(filtered);
-  };
 
   return (
     <ContainerComponent>
-      <View style={[spacing.mh1, { width: SCREEN_WIDTH - 16 }]}>
-        <MyHeader
-          title="Total Earnings"
-          isBack={true}
-          hasIcon={true}
-          icon={"ellipsis-vertical"}
-        />
-        <View
-          style={{
-            flexDirection: "row",
-            marginVertical: 8,
-          }}
-        >
-          <View style={{ width: "80%" }}>
-            <SearchBar
-              placeholder="Search"
-              value={searchText}
-              onChangeText={filterEarnings} 
-            />
-          </View>
-        </View>
-
+      <View>
+        <MyHeader title="Total Earnings" isBack={true} hasIcon={true} />
         <MyFlatList
           data={filteredEarnings}
           keyExtractor={(item) => item.id.toString()}
@@ -58,11 +28,13 @@ const TotalEarningScreen = () => {
               </View>
             </TouchableOpacity>
           )}
+          contentContainerStyle={[spacing.mh2, spacing.mt1]}
+          ListEmptyComponent={() => (
+            <NoRecord msg="Oops! There are no sites data available. Start creating or contact admin" />
+          )}
+          ListHeaderComponent={() => <SearchBar placeholder="Search..." />}
         />
-     
       </View>
     </ContainerComponent>
   );
-};
-
-export default TotalEarningScreen;
+}
