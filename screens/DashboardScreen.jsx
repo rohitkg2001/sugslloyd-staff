fbdee76334a442fffc3a4df28675ab82e598bb0b
Rohit <rgupta6205@gmail.com>
2024-11-22 13:50:47 +0530

    setup reducer

--- screens/DashboardScreen.jsx
+++ screens/DashboardScreen.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import { View, Image, ScrollView, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+import { View, ScrollView, TouchableOpacity } from "react-native";
 import moment from "moment";
 import Icon from "react-native-vector-icons/Ionicons";
 import ContainerComponent from "../components/ContainerComponent";
+import { greet } from "../redux/actions/staffActions";
 import MyFlatList from "../components/utility/MyFlatList";
-import { H3, H5, P } from "../components/text";
+import { H4, H5, P, Span } from "../components/text";
 import CardFullWidth from "../components/card/CardFullWidth";
 import StatCard from "../components/card/Statcard";
 import { useSelector } from "react-redux";
@@ -12,6 +13,7 @@ import { useSelector } from "react-redux";
 import {
   layouts,
   LIGHT,
+  DARK,
   PRIMARY_COLOR,
   SCREEN_WIDTH,
   spacing,
@@ -21,13 +23,20 @@ import {
 import {
   siteCardsForDashboard,
   vendorCardForDashboard,
-  staff,
   tasks,
 } from "../utils/faker";
+import SearchBar from "../components/input/SearchBar";
+import Button from "../components/buttons/Button";
 
 export default function DashboardScreen({ navigation }) {
   const today = useState(moment().format("DD MMM YYYY"));
+  const [dueTasks, setDueTasks] = useState(4);
+  const [greeting, setGreeting] = useState("Good morning");
   const { first_name } = useSelector((state) => state);
+
+  useEffect(() => {
+    setGreeting(greet());
+  }, []);
 
   const navigateToTaskCardScreen = () => {
     navigation.navigate("TaskCardScreen");
@@ -54,17 +63,69 @@ export default function DashboardScreen({ navigation }) {
         ]}
       >
         <View>
-          <H3 style={typography.textBold}>Good Morning, {first_name}</H3>
-          <P style={spacing.ml1}>{today}</P>
+          <H4 style={typography.textBold}>
+            {greeting}, {first_name}
+          </H4>
+          <P style={spacing.ml1}>You have {dueTasks} due Today</P>
         </View>
-        <Image
-          source={{ uri: staff.image }}
-          style={[layouts.circle12, spacing.mv3, layouts.center]}
-        />
+        <TouchableOpacity
+          style={[
+            layouts.circle12,
+            layouts.center,
+            spacing.bw05,
+            spacing.br5,
+            { position: "relative" },
+          ]}
+        >
+          <Icon name="notifications-outline" size={28} color={DARK} />
+          <View
+            style={[
+              styles.bgDanger,
+              layouts.center,
+              {
+                position: "absolute",
+                top: 0,
+                right: 0,
+                height: 24,
+                width: 24,
+                borderRadius: 12,
+              },
+            ]}
+          >
+            <Span
+              style={[
+                typography.textLight,
+                typography.font16,
+                { textAlign: "center" },
+              ]}
+            >
+              6
+            </Span>
+          </View>
+        </TouchableOpacity>
       </View>
-      {/* Welcome message */}
-      <ScrollView>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[spacing.mh1]}
+      >
         <View>
+          <SearchBar
+            placeholder="Search"
+            style={{ width: SCREEN_WIDTH - 70 }}
+          />
+          <Button
+            style={[styles.btn, styles.bgPrimary, spacing.mh1, { width: 50 }]}
+          >
+            <Icon name="options-outline" size={28} color={LIGHT} />
+          </Button>
+          <View style={[spacing.mv2, styles.row, { alignItems: "center" }]}>
+            <H4>Today</H4>
+            <View style={{ flexDirection: "row" }}>
+              <Icon name="calendar-outline" size={24} color={DARK} />
+              <H5 style={spacing.ml1}>{today}</H5>
+            </View>
+          </View>
           <MyFlatList
             data={firstTwoTasks}
             renderItem={({ item }) => (
