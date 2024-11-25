92ce2215f0630f65fdb59c4e176711e4c65b0977
Bittu75469 <bittukr4715@gmail.com>
2024-11-25 14:35:24 +0530

    production cleanup

--- screens/VendorFormScreen.jsx
+++ screens/VendorFormScreen.jsx
@@ -1,91 +1,75 @@
 import React, { useState } from "react";
 import { View, ScrollView } from "react-native";
-import { useDispatch } from "react-redux";
 import ContainerComponent from "../components/ContainerComponent";
 import { SCREEN_WIDTH, spacing, styles } from "../styles";
 import MyHeader from "../components/header/MyHeader";
 import MyTextInput from "../components/input/MyTextInput";
 import MyButton from "../components/buttons/MyButton";
-import { updateVendor } from "../redux/actions/vendorAction";
 
-const VendorFormScreen = ({ navigation, route }) => {
-  const dispatch = useDispatch();
-  const editVendor = route.params?.vendor;
-  const [name, setName] = useState(editVendor?.name || "");
-  const [gstNumber, setGstNumber] = useState(editVendor?.gstNumber || "");
-  const [contactPerson, setContactPerson] = useState(
-    editVendor?.contactPerson || ""
-  );
-  const [contactNumber, setContactNumber] = useState(
-    editVendor?.contactNumber || ""
-  );
-  const [aadharNumber, setAadharNumber] = useState(
-    editVendor?.aadharNumber || ""
-  );
-  const [address, setAddress] = useState(editVendor?.address || "");
-  const [accountName, setAccountName] = useState(editVendor?.accountName || "");
-  const [accountNumber, setAccountNumber] = useState(
-    editVendor?.accountNumber || ""
-  );
-  const [ifsc, setIfsc] = useState(editVendor?.ifsc || "");
-  const [bankName, setBankName] = useState(editVendor?.bankName || "");
-  const [branch, setBranch] = useState(editVendor?.branch || "");
-  const [pan, setPan] = useState(editVendor?.pan || "");
-  const [mailId, setMailId] = useState(editVendor?.mailId || "");
+const VendorFormScreen = () => {
+  const [name, setName] = useState("");
+  const [gstNumber, setGstNumber] = useState("");
+  const [contactPerson, setContactPerson] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [aadharNumber, setAadharNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [accountName, setAccountName] = useState("");
+  const [accountNumber, setAccountNumber] = useState("");
+  const [ifsc, setIfsc] = useState("");
+  const [bankName, setBankName] = useState("");
+  const [branch, SetBranch] = useState("");
+  const [pan, setPan] = useState("");
+  const [mailId, setMailId] = useState("");
 
   const handleCancel = () => {
-    navigation.goBack();
+    setName("");
+    setGstNumber("");
+    setContactPerson("");
+    setContactNumber("");
+    setMailId("");
+    setName("");
+    setAadharNumber("");
+    setAddress("");
+    setAccountName("");
+    setAccountNumber("");
+    setIfsc("");
+    setBankName("");
+    SetBranch("");
+    setPan("");
   };
 
   const handleCreate = () => {
-    const vendorData = {
-      id: editVendor?.id || Date.now().toString(),
-      name,
+    console.log("Creating Vendor with data:", {
       gstNumber,
       contactPerson,
       contactNumber,
-      aadharNumber,
-      address,
-      accountName,
-      accountNumber,
-      ifsc,
-      bankName,
-      branch,
-      pan,
       mailId,
-    };
-    dispatch(updateVendor(vendorData));
-    navigation.goBack();
+    });
   };
-  const { t } = useTranslation();
 
   return (
     <ContainerComponent>
       <ScrollView
         contentContainerStyle={[spacing.mh1, { width: SCREEN_WIDTH - 20 }]}
       >
-        <MyHeader
-          title={editVendor ? "Edit Vendor" : "Create Vendor"}
-          hasIcon={true}
-          isBack={true}
-        />
+        <MyHeader title="Create Vendor" hasIcon={true} isBack={true} />
 
         <MyTextInput
-          title={t("vendor_form_name")}
+          title="Name"
           value={name}
           onChangeText={setName}
           placeholder="Enter Name"
         />
 
         <MyTextInput
-          title={t("contact_person_name")}
+          title="Contact Person"
           value={contactPerson}
           onChangeText={setContactPerson}
           placeholder="Enter Contact Person"
         />
 
         <MyTextInput
-          title={t("vendor_contact_number")}
+          title="Contact Number"
           value={contactNumber}
           onChangeText={setContactNumber}
           placeholder="Enter Contact Number"
@@ -94,13 +78,13 @@ const VendorFormScreen = ({ navigation, route }) => {
 
         <MyTextInput
           title="Aadhar Number"
-          value={aadharNumber}
+          value={contactPerson}
           onChangeText={setAadharNumber}
           placeholder="Enter Aadhar Number"
         />
 
         <MyTextInput
-          title={t("vendor_mail_id")}
+          title="Mail ID"
           value={mailId}
           onChangeText={setMailId}
           placeholder="Enter Mail ID"
@@ -108,49 +92,48 @@ const VendorFormScreen = ({ navigation, route }) => {
         />
 
         <MyTextInput
-          title={t("vendor_address")}
+          title="Address"
           value={address}
           onChangeText={setAddress}
           placeholder="Enter Address"
         />
 
         <MyTextInput
-          title={t("vendor_account_name")}
+          title="Account Name"
           value={accountName}
           onChangeText={setAccountName}
-          placeholder="Enter Account Name"
+          placeholder="Enter Contact Person"
         />
 
         <MyTextInput
-          title={t("vendor_account_number")}
+          title="Account Number"
           value={accountNumber}
           onChangeText={setAccountNumber}
           placeholder="Enter Account Number"
         />
 
         <MyTextInput
-          title={t("ifsc")}
+          title="IFSC"
           value={ifsc}
           onChangeText={setIfsc}
           placeholder="Enter IFSC"
         />
 
         <MyTextInput
-          title={t("bank_name")}
+          title="Bank Name"
           value={bankName}
           onChangeText={setBankName}
           placeholder="Enter Bank Name"
         />
 
         <MyTextInput
-          title={t("branch")}
+          title="Branch"
           value={branch}
-          onChangeText={setBranch}
+          onChangeText={SetBranch}
           placeholder="Enter Branch"
         />
-
         <MyTextInput
-          title={t("gst_number")}
+          title="GST Number"
           value={gstNumber}
           onChangeText={setGstNumber}
           placeholder="Enter GST Number"
@@ -158,17 +141,14 @@ const VendorFormScreen = ({ navigation, route }) => {
 
         <MyTextInput
           title="PAN Number"
-          value={pan}
+          value={bankName}
           onChangeText={setPan}
           placeholder="Enter PAN Number"
         />
       </ScrollView>
       <View style={[styles.row, { width: SCREEN_WIDTH - 20 }]}>
         <MyButton title="Cancel" onPress={handleCancel} color="#DC4C64" />
-        <MyButton
-          title={editVendor ? "Save Changes" : "Create"}
-          onPress={handleCreate}
-        />
+        <MyButton title="Create" onPress={handleCreate} />
       </View>
     </ContainerComponent>
   );
