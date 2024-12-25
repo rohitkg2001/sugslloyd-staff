c4b99ced3e62c6011131369a2280dc9d5d30e3a9
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2024-12-25 09:02:35 +0530

    Sugs lloyd

--- redux/constant.js
+++ redux/constant.js
@@ -1,7 +1,5 @@
 export const LOGIN_STAFF = "LOGIN_STAFF";
-export const BASE_URL = "http://slldm.com";
-//export const BASE_URL = "http://192.168.1.9:8000"
-// export const BASE_URL = "https://peachpuff-grouse-828635.hostingersite.com" //TODO: replace with production server url
+export const BASE_URL = "https://slldm.com";
 
 export const initialState = {};
 
