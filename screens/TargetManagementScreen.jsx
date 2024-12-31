f67c05c24763c2ccd5f4a3fcc73b91a9a3966bd9
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2024-12-31 10:43:27 +0530

    message

--- screens/TargetManagementScreen.jsx
+++ screens/TargetManagementScreen.jsx
@@ -35,18 +35,20 @@ const TargetManagementScreen = ({ route, navigation }) => {
 
   const getCurrentTask = async () => {
     const thisTask = await getTaskById(id);
-    setCurrentTarget(thisTask);
+    setCurrentTarget(thisTask.task);
   };
   useEffect(() => {
+    console.log("Fetching Details")
     getCurrentTask();
     dispatch(getAllVendors());
+    console.log("Details Fdtched")
   }, []);
 
-  const isDataAvailable = target && Object.keys(target).length > 0;
+  // const isDataAvailable = currentTarget && Object.keys(currentTarget).length > 0;
   const closeTaskInventoryScreen = () => setShowTaskInventory(false);
 
   useEffect(() => {
-
+    console.log(currentTarget)
   }, [])
 
 
@@ -54,88 +56,70 @@ const TargetManagementScreen = ({ route, navigation }) => {
     <ContainerComponent>
       <MyHeader title={t("Task")} hasIcon={true} isBack={true} />
 
-      <View style={{ width: SCREEN_WIDTH - 16 }}>
-        {isDataAvailable ? (
-          <ScrollView>
-            <H5
-              style={{
-                textAlign: "right",
-                marginRight: 20,
-                textTransform: "uppercase",
-              }}
-            >
-              {currentTarget.activity}
-            </H5>
-            <H4>{currentTarget.site?.site_name}</H4>
-            <H6>{currentTarget.site?.location}</H6>
+      <ScrollView contentContainerStyle={{ width: SCREEN_WIDTH - 16 }}>
+        <H5
+          style={{
+            textAlign: "right",
+            marginRight: 20,
+            textTransform: "uppercase",
+          }}
+        >
+          {currentTarget.activity}
+        </H5>
+        <H4>{currentTarget.site?.site_name}</H4>
+        <H6>{currentTarget.site?.location}</H6>
+        <H6>
+          {currentTarget.start_date} - {currentTarget.end_date}
+        </H6>
+        <View
+          style={[
+            styles.row,
+            spacing.mv2,
+            { justifyContent: "space-between", flex: 1 },
+          ]}
+        >
+          <H6>Vendor Name</H6>
+          <View style={[styles.row, { alignItems: "center" }]}>
             <H6>
-              {currentTarget.start_date} - {currentTarget.end_date}
+              {!currentTarget.vendor
+                ? "................................."
+                : currentTarget.vendor?.name}
             </H6>
-            <View
-              style={[
-                styles.row,
-                spacing.mv2,
-                { justifyContent: "space-between", flex: 1 },
-              ]}
-            >
-              <H6>Vendor Name</H6>
-              <View style={[styles.row, { alignItems: "center" }]}>
-                <H6>
-                  {!currentTarget.vendor
-                    ? "................................."
-                    : currentTarget.vendor?.name}
-                </H6>
-                <IconButton
-                  onPress={() => setShowVendorSelection(true)}
-                  icon={"pencil"}
-                />
-              </View>
-            </View>
-            <View
-              style={[
-                styles.row,
-                spacing.mv2,
-                { justifyContent: "space-between", flex: 1 },
-              ]}
-            >
-              <H6>Remarks</H6>
-              <View style={[styles.row, { alignItems: "center" }]}>
-                <H6>
-                  {!currentTarget.description
-                    ? "................................."
-                    : currentTarget.description}
-                </H6>
-                <IconButton
-                  onPress={() => setShowVendorSelection(true)}
-                  icon={"pencil"}
-                />
-              </View>
-            </View>
+            <IconButton
+              onPress={() => setShowVendorSelection(true)}
+              icon={"pencil"}
+            />
+          </View>
+        </View>
+        <View
+          style={[
+            styles.row,
+            spacing.mv2,
+            { justifyContent: "space-between", flex: 1 },
+          ]}
+        >
+          <H6>Remarks</H6>
+          <View style={[styles.row, { alignItems: "center" }]}>
+            <H6>
+              {!currentTarget.description
+                ? "................................."
+                : currentTarget.description}
+            </H6>
+            <IconButton
+              onPress={() => setShowVendorSelection(true)}
+              icon={"pencil"}
+            />
+          </View>
+        </View>
 
-            {currentTarget.image && (
-              <View style={[spacing.pv4]}>
-                <H6>Completed Photos</H6>
-                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-                  {target.completedPhotos.map((url, index) => (
-                    <Image
-                      key={index}
-                      source={{ uri: url }}
-                      style={{ width: 100, height: 100, marginRight: 10 }}
-                    />
-                  ))}
-                </ScrollView>
-              </View>
-            )}
-          </ScrollView>
-        ) : (
-          <NoRecord msg="No data found" />
-        )}
+      </ScrollView>
 
-        <Tabs
-          tabs={[t("Inventory"), t("Progress")]}
-          onTabPress={(index) => setActiveTab(index)}
-        />
-      </View>
+
+      {/* <Tabs
+        tabs={[t("Inventory"), t("Progress")]}
+        onTabPress={(index) => setActiveTab(index)}
+      /> */}
+      {/* </View> */}
 
       <Button
         style={styles.addButton}
