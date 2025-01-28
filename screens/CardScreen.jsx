e2faaa696e9f8f3685c2216ad62b0ba7e4667bba
Rohit <rgupta6205@gmail.com>
2025-01-28 11:55:44 +0530

    first commit

--- screens/CardScreen.jsx
+++ screens/CardScreen.jsx
@@ -1,3 +1,84 @@
+// import { View, Image, TouchableOpacity } from "react-native";
+// import { Card } from "react-native-paper";
+// import ContainerComponent from "../components/ContainerComponent";
+// import { spacing, typography, SCREEN_WIDTH } from "../styles";
+// import { P } from "../components/text";
+
+// export default function ({ navigation }) {
+//   return (
+//     <ContainerComponent justifyContent="space-between" paddingVertical={20}>
+//       <Image
+//         source={require("../assets/adaptive-icon.png")}
+//         style={{ height: 100, resizeMode: "contain" }}
+//       />
+//       <View>
+//         <TouchableOpacity onPress={() => navigation.navigate("loginScreen")}>
+//           <Card
+//             style={[
+//               spacing.mt5,
+//               {
+//                 width: SCREEN_WIDTH * 0.9,
+//                 backgroundColor: "#f8f9fa",
+//               },
+//             ]}
+//           >
+//             <Image
+//               source={require("../assets/rooft.png")}
+//               style={[
+//                 spacing.mt2,
+//                 {
+//                   width: "70%",
+//                   height: 70,
+//                   alignSelf: "center",
+//                 },
+//               ]}
+//             />
+//             <View style={[spacing.p3]}>
+//               <P style={[typography.font16, { textAlign: "center" }]}>
+//                 Rooftop Installation
+//               </P>
+//             </View>
+//           </Card>
+//         </TouchableOpacity>
+
+//         <TouchableOpacity onPress={() => navigation.navigate("loginScreen")}>
+//           <Card
+//             style={[
+//               spacing.mt5,
+//               {
+//                 width: SCREEN_WIDTH * 0.9,
+//                 backgroundColor: "#e0f7fa",
+//                 alignSelf: "center",
+//               },
+//             ]}
+//           >
+//             <Image
+//               source={require("../assets/solar.png")}
+//               style={[
+//                 spacing.mt3,
+//                 {
+//                   width: "80%",
+//                   height: 80,
+//                   alignSelf: "center",
+//                 },
+//               ]}
+//             />
+//             <View style={[spacing.p3]}>
+//               <P style={[typography.font16, { textAlign: "center" }]}>
+//                 Streetlight Installation
+//               </P>
+//             </View>
+//           </Card>
+//         </TouchableOpacity>
+//       </View>
+
+//       <P style={[typography.font16, { color: "black" }]}>
+//         Powered by Dashandots Technology
+//       </P>
+//     </ContainerComponent>
+//   );
+// }
+
 import { View, Image, TouchableOpacity } from "react-native";
 import { Card } from "react-native-paper";
 import ContainerComponent from "../components/ContainerComponent";
@@ -5,6 +86,10 @@ import { spacing, typography, SCREEN_WIDTH } from "../styles";
 import { P } from "../components/text";
 
 export default function ({ navigation }) {
+  const handlePress = (nextScreen) => {
+    navigation.navigate("loginScreen", { nextScreen });
+  };
+
   return (
     <ContainerComponent justifyContent="space-between" paddingVertical={20}>
       <Image
@@ -12,7 +97,7 @@ export default function ({ navigation }) {
         style={{ height: 100, resizeMode: "contain" }}
       />
       <View>
-        <TouchableOpacity onPress={() => navigation.navigate("loginScreen")}>
+        <TouchableOpacity onPress={() => handlePress("homeScreen")}>
           <Card
             style={[
               spacing.mt5,
@@ -41,7 +126,8 @@ export default function ({ navigation }) {
           </Card>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => navigation.navigate("loginScreen")}>
+        {/* Streetlight Installation */}
+        <TouchableOpacity onPress={() => handlePress("welcomeScreen")}>
           <Card
             style={[
               spacing.mt5,
--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,3 +1,121 @@
+// import { useState } from "react";
+// import {
+//   KeyboardAvoidingView,
+//   View,
+//   Platform,
+//   ScrollView,
+//   Text,
+//   TouchableOpacity,
+// } from "react-native";
+// import MyImageBackground from "../components/MyImageBackground";
+// import { H1, H5, Span, H2 } from "../components/text";
+// import MyTextInput from "../components/input/MyTextInput";
+// import Button from "../components/buttons/Button";
+// import { layouts, spacing, typography, styles, ICON_LARGE } from "../styles";
+// import { useDispatch } from "react-redux";
+// import Icon from "react-native-vector-icons/Ionicons";
+// import { login } from "../redux/actions/staffActions";
+// import { fakeDelete } from "../utils/faker";
+// import { useTranslation } from "react-i18next";
+
+// export default function LoginScreen({ navigation }) {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+//   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+//   const dispatch = useDispatch();
+//   const { t } = useTranslation();
+
+//   const onSubmit = async () => {
+//     setError("");
+//     try {
+//       const result = await dispatch(login(username, password));
+//       if (result) {
+//         navigation.navigate("homeScreen");
+//       } else {
+//         setError("Please provide the correct credentials");
+//       }
+//     } catch (error) {
+//       setError("An error occurred during login");
+//     }
+//   };
+
+//   const togglePasswordVisibility = () => {
+//     setIsPasswordVisible(!isPasswordVisible);
+//   };
+
+//   return (
+//     <MyImageBackground imageSource={require("../assets/Login.png")}>
+//       <ScrollView style={{ flex: 1 }}>
+//         <View style={[layouts.center, spacing.mv5]}>
+//           <H1 style={spacing.mv2}>{t("loginTitle")}</H1>
+//           <H5 style={spacing.mb5}>{t("loginSubtitle")}</H5>
+//         </View>
+//         <KeyboardAvoidingView
+//           behavior={Platform.OS === "ios" ? "padding" : "height"}
+//           style={spacing.mb5}
+//         >
+//           <MyTextInput
+//             title="Username"
+//             type="email"
+//             placeholder="abc@xyz.com"
+//             value={username}
+//             onChangeText={setUsername}
+//           />
+//           <View style={[styles.passwordContainer, { position: "relative" }]}>
+//             <MyTextInput
+//               title="Password"
+//               type="password"
+//               secureTextEntry={!isPasswordVisible}
+//               value={password}
+//               onChangeText={setPassword}
+//             />
+//             <TouchableOpacity
+//               style={{
+//                 position: "absolute",
+//                 right: spacing.mr2.marginRight,
+//                 top: 40,
+//               }}
+//               onPress={togglePasswordVisibility}
+//             >
+//               <Icon
+//                 name={isPasswordVisible ? "eye-off" : "eye"}
+//                 size={ICON_LARGE}
+//                 color="gray"
+//               />
+//             </TouchableOpacity>
+//           </View>
+
+//           {error ? (
+//             <Text style={{ color: "red", marginBottom: 10 }}>{error}</Text>
+//           ) : null}
+
+//           <TouchableOpacity
+//             onPress={() =>
+//               fakeDelete({
+//                 title: "Forgot Password",
+//                 message:
+//                   "Forgot your password? Don’t worry—we’re here to help! Please contact your admin for assistance with resetting your password and getting back into your account quickly",
+//                 positiveText: "OK",
+//               })
+//             }
+//           >
+//             <Span style={styles.rightLink}>{t("forgotPasswordText")}</Span>
+//           </TouchableOpacity>
+//         </KeyboardAvoidingView>
+//         <Button
+//           style={[styles.btn, styles.bgPrimary, { justifyContent: "center" }]}
+//           onPress={onSubmit}
+//         >
+//           <H2 style={[styles.btnText, styles.textLarge, typography.textLight]}>
+//             {t("loginBtnText")}
+//           </H2>
+//         </Button>
+//       </ScrollView>
+//     </MyImageBackground>
+//   );
+// }
+
 import { useState } from "react";
 import {
   KeyboardAvoidingView,
@@ -18,7 +136,7 @@ import { login } from "../redux/actions/staffActions";
 import { fakeDelete } from "../utils/faker";
 import { useTranslation } from "react-i18next";
 
-export default function LoginScreen({ navigation }) {
+export default function LoginScreen({ navigation, route }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -26,12 +144,14 @@ export default function LoginScreen({ navigation }) {
   const dispatch = useDispatch();
   const { t } = useTranslation();
 
+  const nextScreen = route.params?.nextScreen || "homeScreen";
+
   const onSubmit = async () => {
     setError("");
     try {
       const result = await dispatch(login(username, password));
       if (result) {
-        navigation.navigate("welcomeScreen");
+        navigation.navigate(nextScreen);
       } else {
         setError("Please provide the correct credentials");
       }
