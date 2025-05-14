8b943881e441263ce6caf63ef2497c7fc4c90ca9
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2025-05-15 00:31:32 +0530

    message

--- store.js
+++ store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import reducer from "./redux/reducers";
 
 const store = createStore(reducer, applyMiddleware(thunk));
