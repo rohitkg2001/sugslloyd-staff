26981cf0a71a7b775f163cfe69070f89b3fee86f
Rohit <rgupta6205@gmail.com>
2025-06-11 14:51:34 +0530

    upgrade version

--- screens/AddBillForm.jsx
+++ screens/AddBillForm.jsx
@@ -344,10 +344,6 @@ const AddBillForm = ({ navigation }) => {
     try {
       const payload = {
         ...form,
-        // total_km: Number(form.total_km),
-        //  rate_per_km: Number(form.rate_per_km),
-        // Rent: Number(form.Rent),
-        // add_rent: Number(form.add_rent) || 0, // Ensure add_rent is included
         travelfare: formFields.map((field) => ({
           from: field.from || "",
           to: field.to || "",
@@ -427,7 +423,7 @@ const AddBillForm = ({ navigation }) => {
             )}
 
             {/* Departure Date */}
-            <H6>Departure Date</H6>
+            <Span style={[typography.fontLato]}>Departure Date</Span>
             <Pressable
               onPress={() => showDatePicker("departure_time")}
               style={[
@@ -452,7 +448,7 @@ const AddBillForm = ({ navigation }) => {
             </Pressable>
 
             {/* Return Date */}
-            <H6>Return Date</H6>
+            <Span style={[typography.fontLato]}>Return Date</Span>
             <Pressable
               onPress={() => showDatePicker("date_of_return")}
               style={[
@@ -607,7 +603,6 @@ const AddBillForm = ({ navigation }) => {
                       placeholder="Enter To Location"
                       inputStyle={{ width: "100%" }}
                     />
-
                     <MyPickerInput
                       title={"Mode Of Transport"}
                       value={form.mode_of_transport}
@@ -837,7 +832,7 @@ const AddBillForm = ({ navigation }) => {
                 {/* YES section */}
                 {entry.is_guest_house_available === "Yes" && (
                   <>
-                    <H6>Check In Date</H6>
+                    <Span style={typography.fontLato}>Check In Date</Span>
                     <Pressable
                       onPress={() =>
                         handleOpenDatePicker(index, "check_in_date")
@@ -865,7 +860,7 @@ const AddBillForm = ({ navigation }) => {
                       <Icon name="calendar" size={20} color="#888" />
                     </Pressable>
 
-                    <H6>Check Out Date</H6>
+                    <Span style={typography.fontLato}>Check Out Date</Span>
                     <Pressable
                       onPress={() =>
                         handleOpenDatePicker(index, "check_out_date")
@@ -953,7 +948,7 @@ const AddBillForm = ({ navigation }) => {
                       </View>
                     )}
 
-                    <H6>Check In Date</H6>
+                    <Span style={typography.fontLato}>Check In Date</Span>
                     <Pressable
                       onPress={() =>
                         handleOpenDatePicker(index, "check_in_date")
@@ -981,7 +976,7 @@ const AddBillForm = ({ navigation }) => {
                       <Icon name="calendar" size={20} color="#888" />
                     </Pressable>
 
-                    <H6>Check Out Date</H6>
+                    <Span style={typography.fontLato}>Check Out Date</Span>
                     <Pressable
                       onPress={() =>
                         handleOpenDatePicker(index, "check_out_date")
@@ -1193,7 +1188,7 @@ const AddBillForm = ({ navigation }) => {
                 />
 
                 {/* Date of Expense Picker */}
-                <H6>Date of Expense</H6>
+                <Span style={typography.fontLato}>Date of Expense</Span>
                 <Pressable
                   onPress={() => openExpenseDatePicker(index)}
                   style={[
